d9ab1439df6bc38f3235ce194f1584480b1b0da6
Mii-Muu <spotinfotech.in@gmail.com>
2023-06-28 21:26:32 +0530

    sofa commit

---
+++ js/sagTusEngelle.js
@@ -0,0 +1,1 @@
+﻿document.oncontextmenu=function(e){var t=e||window.event;var n=t.target||t.srcElement;if(n.nodeName!="A")return false}; document.ondragstart=function(){return false}; var sc_project=11640569;var sc_invisible=1;var sc_security="833fec06";var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www."); document.write("");document.write('');
